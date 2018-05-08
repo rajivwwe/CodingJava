66e1dd8af7448bf28a01ed4859caca9211735363
rajivwwe <38311130+rajivwwe@users.noreply.github.com>
2018-05-08 09:56:37 +0530

    Palindrome File with some code.

---
+++ PalindromeNumber.java
@@ -0,0 +1,11 @@
+# CodingJava
+import java.util.Scanner;
+public class PalinDromeNumber
+{
+public static void main(String args[]){
+System.out.println("Enter a number");
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int a,b,i;
+
+}
